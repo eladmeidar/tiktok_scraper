58281795a4a354894d82029cab55fc30a6a9c37a
Lord Meidar <elad@shinobidevs.com>
2021-05-03 16:07:54 +0300

    [FIX]

--- lib/tiktok_scraper/hashtag.rb
+++ lib/tiktok_scraper/hashtag.rb
@@ -7,12 +7,16 @@ module TiktokScraper
     def self.get_posts(hashtag, count = 100)
       videos = []
       
-      result = get_hashtag_posts(hashtag)
+      hashtag_id = get_hashtag_id(hashtag)
+      puts "HASHTAG ID: #{hashtag_id}"
 
+      result = get_hashtag_posts(hashtag_id)
+      
       videos = videos.concat(result[:items])
 
       while result[:has_more] && videos.length <= count
-        result = get_hashtag_posts(hashtag, result[:cursor])
+        result = get_hashtag_posts(hashtag_id, result[:cursor])
+      
         videos = videos.concat(result[:items])
       end
       
@@ -21,16 +25,16 @@ module TiktokScraper
 
     protected
 
-    def self.get_hashtag_posts(hashtag, cursor = nil)
+    def self.get_hashtag_posts(hashtag_id, cursor = nil)
       query = {
-        name: hashtag
+        id: hashtag_id
       }
 
       if cursor
         query[:cursor] = cursor
       end
 
-      results = TiktokScraper.get('/public/hashtag', query: query)
+      results = TiktokScraper.get('/public/hashtag', query: query).parsed_response
       
       return {
         cursor: results["cursor"],
@@ -40,14 +44,13 @@ module TiktokScraper
     end
 
     def self.get_hashtag_id(hashtag)
-      raw_results = TiktokScraper.get("/public/discover/hashtag", query: {keyword: hashtag, count: MAX_POSTS_PER_PAGE}).parsed_response
-      id = raw_results["challengeInfoList"].first["challenge"]["id"]
-      hashtag_name = raw_results["challengeInfoList"].first["challenge"]["title"]
-      if id.nil? || hashtag_name.downcase != hashtag.downcase
-        raise TiktokScraper::HashtagNotFound, "Hashtag '#{hashtag}' was not found"
-      else
-        id
-      end
+      query = {
+        name: hashtag
+      }
+
+      results = TiktokScraper.get('/public/hashtag', query: query).parsed_response
+
+      return results["challenge"]["id"]
     end
   end
 end
